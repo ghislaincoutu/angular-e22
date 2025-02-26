08ad155d85aab85ffabf816859ac99e0ba468f74
gcoutu <gc8395@gmail.com>
2025-02-25 19:12:20 -0500

    Exercice Angular

--- related_files/scripts/publish_application.sh
+++ related_files/scripts/publish_application.sh
@@ -8,7 +8,7 @@ echo "Publication de l'applications sur le serveur Web local"
 apply_pause "Appuyer sur la touche [Retour] pour continuer..."
 
 # Source (so)
-so=/home/Exercises/angular-e22/dist/angular-e22/browser
+so=/home/xd21/angular-e22/dist/angular-e22/browser
 # Destination (de)
 de=/var/www/html/d002/angular-e22
 
