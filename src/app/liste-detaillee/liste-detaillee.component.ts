8f8c3cfc0e604f9d3a1b0f0a65c5d1da68741698
gcoutu <gc8395@gmail.com>
2026-01-23 23:22:44 -0500

    Exercice Angular

--- src/app/liste-detaillee/liste-detaillee.component.ts
+++ src/app/liste-detaillee/liste-detaillee.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 import { Module01Service } from '../module01.service';
 import { Observable } from 'rxjs';
 
+import { Aide001Component } from '../fiches/aide001/aide001.component';
+import { Aide002Component } from '../fiches/aide002/aide002.component';
+import { Aide003Component } from '../fiches/aide003/aide003.component';
+import { Aide004Component } from '../fiches/aide004/aide004.component';
+import { Gram001Component } from '../fiches/gram001/gram001.component';
+import { Gram002Component } from '../fiches/gram002/gram002.component';
+import { Phon001Component } from '../fiches/phon001/phon001.component';
+import { Phon002Component } from '../fiches/phon002/phon002.component';
+
 interface Notions55 {
   id: string;
 }
@@ -12,6 +21,7 @@ interface Notions55 {
   styleUrls: ['./liste-detaillee.component.scss'],
   standalone: false
 })
+
 export class ListeDetailleeComponent {
   notions81!: Observable<any[]>;
   notions56: Notions55[] = [];
@@ -23,6 +33,17 @@ export class ListeDetailleeComponent {
   gram002: string = '';
   phon001: string = '';
   phon002: string = '';
+
+  componentMap: Record<string, any> = {
+    'aide001': Aide001Component,
+    'aide002': Aide002Component,
+    'aide003': Aide003Component,
+    'aide004': Aide004Component,
+    'gram001': Gram001Component,
+    'gram002': Gram002Component,
+    'phon001': Phon001Component,
+    'phon002': Phon002Component,
+  };
 
   constructor(private Module01: Module01Service) {
     this.notions56 = [
